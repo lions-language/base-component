0dedac9bd255a9252e0b93dfe36e6f6580d5cbc9
MwlLj <731025894@qq.com>
2020-12-07 16:04:14 +0800

    实现 register Any

--- src/flag/mod.rs
+++ src/flag/mod.rs
@@ -2,23 +2,39 @@ use std::env;
 use std::rc::Rc;
 use std::cell::RefCell;
 use std::collections::HashMap;
+use std::any::{Any};
+use std::fmt::Display;
 
-struct Item(Rc<RefCell<String>>, String, bool);
+type FromString<V: Any> = fn(value: String) -> V;
 
-pub struct Cmd {
-    help: String,
-    keys: HashMap<String, Item>
+struct Item<V: Any + Display> {
+    value: Rc<RefCell<V>>,
+    from: FromString<V>,
+    desc: String,
+    is: bool
 }
 
-impl Cmd {
-    pub fn register(&mut self, key: &str, default: &str) -> Rc<RefCell<String>> {
-        self.register_with_desc(key, default, "")
+pub struct Flag<V: Any + Display> {
+    help: String,
+    keys: HashMap<String, Item<V>>
+}
+
+impl<V: Any + Display> Flag<V> {
+    pub fn register(&mut self, key: String, default: V
+        , from: FromString<V>) -> Rc<RefCell<V>> {
+        self.register_with_desc(key, default, from, String::from(""))
     }
 
-    pub fn register_with_desc(&mut self, key: &str, default: &str, desc: &str) -> Rc<RefCell<String>> {
-        let r = Rc::new(RefCell::new(default.to_string()));
-        self.keys.insert(key.to_string(), Item(r.clone(), desc.to_string(), false));
-        r.clone()
+    pub fn register_with_desc(&mut self, key: String, default: V
+        , from: FromString<V>, desc: String) -> Rc<RefCell<V>> {
+        let r = Rc::new(RefCell::new(default));
+        self.keys.insert(key.to_string(), Item{
+            value: r.clone(),
+            from: from,
+            desc: desc.to_string(),
+            is: false
+        });
+        r
     }
 
     pub fn has(&self, key: &str) -> bool {
@@ -28,14 +44,14 @@ impl Cmd {
                 return false;
             }
         };
-        v.2
+        v.is
     }
 
     pub fn parse(&mut self) {
         let args = env::args();
-        let argsLen = args.len();
-        let mut isFind = false;
-        let mut lastKey = "".to_string();
+        let args_len = args.len();
+        let mut is_find = false;
+        let mut last_key = "".to_string();
         for (index, arg) in args.enumerate() {
             if arg == self.help {
                 self.printHelp();
@@ -43,30 +59,28 @@ impl Cmd {
             }
             match self.keys.get(&arg) {
                 Some(field) => {
-                    isFind = true;
-                    lastKey = arg;
-                    if let Some(r) = self.keys.get_mut(&lastKey) {
-                        r.2 = true;
+                    is_find = true;
+                    last_key = arg;
+                    if let Some(r) = self.keys.get_mut(&last_key) {
+                        r.is = true;
                     };
                 },
                 None => {
-                    if isFind == true {
-                        if let Some(r) = self.keys.get_mut(&lastKey) {
-                            *(*r.0/*get HashMap mut value*/).borrow_mut() = arg;
+                    if is_find == true {
+                        if let Some(r) = self.keys.get_mut(&last_key) {
+                            *(*r.value).borrow_mut() = (r.from)(arg);
                         }
                     }
-                    isFind = false;
+                    is_find = false;
                 }
             }
         }
     }
-}
 
-impl Cmd {
     fn printHelp(&self) {
         println!("help:");
         for (key, value) in self.keys.iter() {
-            println!("\t{}\n\t\tdefault: {}\n\t\tdesc: {}", key, *value.0.borrow(), &value.1);
+            println!("\t{}\n\t\tdefault: {}\n\t\tdesc: {}", key, *value.value.borrow(), &value.desc);
         }
     }
 
@@ -77,9 +91,7 @@ impl Cmd {
             std::process::exit(0);
         }
     }
-}
 
-impl Cmd {
     pub fn new() -> Self {
         Self {
             help: "--help".to_string(),
@@ -87,3 +99,4 @@ impl Cmd {
         }
     }
 }
+
