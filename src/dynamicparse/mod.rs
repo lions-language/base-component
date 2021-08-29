58ca5f73886c954f122195e0cbb280451a4861d9
lions-langugae <lions_language@163.com>
2021-08-29 11:18:52 +0800

    add next function-pointer

--- src/dynamicparse/mod.rs
+++ src/dynamicparse/mod.rs
@@ -12,36 +12,82 @@ pub enum ValueReaderStatus {
     Ready
 }
 
-pub trait ValueReader {
-    fn next(&self) -> ValueReaderStatus;
-    fn to_any(self) -> Box<Any>;
+pub struct ValueReader {
+    pub next: fn(&str) -> ValueReaderStatus
 }
 
 struct Item {
     desc: String,
-    value: Value
+    value: Value,
+    reader: ValueReader
 }
 
 pub struct Command {
+    help_key: String,
     keys: HashMap<String, Item>
 }
 
 impl Command {
-    pub fn register<T: ValueReader>(
-        &mut self, key: &str, default: T, desc: &str) -> Value {
+    pub fn register_with_reader(
+        &mut self, key: &str, default: Box<Any>, desc: &str, reader: ValueReader) -> Value {
 
-        let value = Rc::new(RefCell::new(default.to_any()));
+        let value = Rc::new(RefCell::new(default));
 
         self.keys.insert(key.to_string(), Item{
             desc: desc.to_string(),
-            value: value.clone()
+            value: value.clone(),
+            reader: reader
         });
 
         value
     }
 
+    pub fn parse(&mut self) {
+        let args = env::args();
+
+        for (_, arg) in args.enumerate() {
+            if arg == self.help_key {
+                self.print_help();
+                self.exit();
+            }
+
+            let item = match self.keys.get(&arg) {
+                Some(item) => item,
+                None => {
+                    continue;
+                }
+            };
+
+            loop {
+                match (item.reader.next)(&arg) {
+                    ValueReaderStatus::Pending => {
+                    },
+                    ValueReaderStatus::Ready => {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    fn print_help(&self) {
+        println!("help:");
+        for (key, value) in self.keys.iter() {
+            // println!("\t{}\n\t\tdefault: {}\n\t\tdesc: {}", key, value.value, &value.desc);
+        }
+    }
+
+    fn exit(&self) {
+        std::process::exit(0);
+    }
+
+    pub fn set_help_key(&mut self, key: String) {
+        *&mut self.help_key = key;
+    }
+
     pub fn new() -> Self {
         Self {
+            help_key: String::new(),
             keys: HashMap::new()
         }
     }
